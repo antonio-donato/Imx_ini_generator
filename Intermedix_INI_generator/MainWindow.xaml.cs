518f1c8b651e16e17fbd188a51fc255cfb5087f8
Antonio Donato <donant@gmail.com>
2017-12-01 17:06:18 +0100

    First commit

---
+++ Intermedix_INI_generator/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Intermedix_INI_generator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string imgName = null;
+        private Visibility visCheckOrder = Visibility.Hidden;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        public void CreteIni()
+        {
+            var MyIni = new IniFile("Test.ini");
+
+            MyIni.Write("DESCR", textCampaignDescription.Text, textCampaignName.Text);
+            MyIni.Write("TIPO", (comboCampaignType.SelectedItem as ComboBoxItem).Tag.ToString(), textCampaignName.Text);
+            MyIni.Write("IMG", @"dat\prodcons\" + imgName, textCampaignName.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CreteIni();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string testo1 = (sender as TextBox).Text;
+            //var testo = textProdottoPerGiacenza.Text;
+
+            if (testo1 != String.Empty)
+            {
+                visCheckOrder = Visibility.Visible;
+            }
+
+            //chkOrderButton.Visibility = visCheckOrder;
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //string gettag = (comboCampaignType.SelectedItem as ComboBoxItem).Tag.ToString();
+            //MessageBox.Show(gettag);
+        }
+
+        private void btnImg_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".jpg";
+            dlg.Filter = "JPG Files (*.jpg)|*.jpg";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                imgName = dlg.SafeFileName;
+
+                //Uri uriDoc = new Uri(dlg.FileName, UriKind.Relative);
+                Uri uriDoc = new Uri(dlg.FileName);
+
+                imgVis.Source = new BitmapImage(uriDoc);
+                btnImg.Content = imgName;
+            }
+        }
+    }
+}
